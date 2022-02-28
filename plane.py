85615ee0ffd59cd475ecbc0161c9415282c87fbb
Owen Brooks <owen.h.brooks@gmail.com>
2022-02-28 22:57:23 +1100

    Remove outliers using statistical and radial methods

--- plane.py
+++ plane.py
@@ -1,42 +1,44 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import open3d as o3d
 import sys
 
+# Load pcd
 print("Loading pcd")
 filename = sys.argv[1]
 pcd = o3d.io.read_point_cloud(filename)
 
+# Downsample pcd
 downpcd = pcd.voxel_down_sample(voxel_size=0.05)
 pcd = downpcd
-#o3d.visualization.draw_geometries([pcd], point_show_normal=True)
+# o3d.visualization.draw_geometries([pcd], point_show_normal=True)
 
-points = np.asarray(pcd.points)
-#new_points = points[points[:, 1] < float(sys.argv[2])]
-#pcd.points = o3d.utility.Vector3dVector(new_points)
-
+# Filter out for only points that have close to horizontal normals
 normals = np.asarray(downpcd.normals)
-print(normals)
-
-# try to filter out for only points that have close to horizontal normals
 vertical = np.array([0.0, 1.0, 0.0])
 dot = np.array([np.dot(vertical, norm) for norm in normals])
-new_points = points[dot > 0.8]
-#pcd.points = o3d.utility.Vector3dVector(new_points)
 horz_norms = np.arange(len(dot))[np.abs(dot)<0.1]
 pcd = pcd.select_by_index(horz_norms)
-print(len(points))
-print(len(new_points))
-o3d.visualization.draw_geometries([pcd], point_show_normal=False)
-# plane detection
-plane_model, inliers = pcd.segment_plane(distance_threshold=0.1,
-                                         ransac_n=3,
-                                         num_iterations=1000)
-[a, b, c, d] = plane_model
-print(f"Plane equation: {a:.2f}x + {b:.2f}y + {c:.2f}z + {d:.2f} = 0")
+# o3d.visualization.draw_geometries([pcd], point_show_normal=False)
 
-#inlier_cloud = pcd.select_by_index(inliers)
-#inlier_cloud.paint_uniform_color([1.0, 0, 0])
-#outlier_cloud = pcd.select_by_index(inliers, invert=True)
-#o3d.visualization.draw_geometries([inlier_cloud, outlier_cloud])
-#o3d.visualization.draw_geometries([outlier_cloud], point_show_normal=True)
+def display_inlier_outlier(cloud, ind):
+    inlier_cloud = cloud.select_by_index(ind)
+    outlier_cloud = cloud.select_by_index(ind, invert=True)
+
+    print("Showing outliers (red) and inliers (gray): ")
+    outlier_cloud.paint_uniform_color([1, 0, 0])
+    inlier_cloud.paint_uniform_color([0.8, 0.8, 0.8])
+    o3d.visualization.draw_geometries([inlier_cloud, outlier_cloud])
+
+# Remove outliers
+print("Statistical oulier removal")
+cl, ind = pcd.remove_statistical_outlier(nb_neighbors=300,
+                                                    std_ratio=2.0)
+display_inlier_outlier(pcd, ind)
+in_pcd = pcd.select_by_index(ind)
+o3d.visualization.draw_geometries([in_pcd])
+
+print("Radius outlier removal")
+cl, ind = pcd.remove_radius_outlier(nb_points=30, radius=0.15)
+display_inlier_outlier(pcd, ind)
+in_pcd = pcd.select_by_index(ind)
+o3d.visualization.draw_geometries([in_pcd])
