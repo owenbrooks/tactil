84784c871275b9f9883e57bb99525fc8946fa185
Owen Brooks <owen.h.brooks@gmail.com>
2022-03-10 22:39:43 +1100

    Collapse into 2D

--- multiplane.py
+++ multiplane.py
@@ -14,20 +14,30 @@ print("Loaded pcd")
 # Downsample pcd
 pcd = pcd.voxel_down_sample(voxel_size=0.05)
 print("Downsampled pcd")
-# o3d.visualization.draw_geometries([downpcd], point_show_normal=False)
+# o3d.visualization.draw_geometries([pcd], point_show_normal=False)
 
 
 # Max vertical threhold
 points = np.asarray(pcd.points)
-remaining = points[:, 1] < 2.0
+remaining = points[:, 1] < 2.2
 pcd = pcd.select_by_index(np.arange(len(remaining))[remaining])
 print(np.asarray(pcd.points).shape)
+# o3d.visualization.draw_geometries([pcd], point_show_normal=False)
+
+# # Perform plane segmentation to filter out ground points
+# ground_plane, inliers = pcd.segment_plane(
+#     distance_threshold=0.05,ransac_n=3,num_iterations=1000)
+# ground = pcd.select_by_index(inliers)
+# ground.paint_uniform_color([1.0, 0.0, 0.0])
+# # o3d.visualization.draw_geometries([pcd], point_show_normal=False)
+# pcd = pcd.select_by_index(inliers, invert=True)
+# # o3d.visualization.draw_geometries([pcd], point_show_normal=False)
 
 # Filter out for only points that have close to horizontal normals
 normals = np.asarray(pcd.normals)
 vertical = np.array([0.0, 1.0, 0.0])
 dot = np.array([np.dot(vertical, norm) for norm in normals])
-horz_norms = np.arange(len(dot))[np.abs(dot)<0.1]
+horz_norms = np.arange(len(dot))[np.abs(dot)<0.2]
 pcd = pcd.select_by_index(horz_norms)
 print(f"Filtered for horizontal normals, new length: {np.asarray(pcd.points).shape[0]}")
 # o3d.visualization.draw_geometries([pcd], point_show_normal=False)
@@ -43,14 +53,14 @@ print(f"Point cloud has {max_label + 1} clusters")
 colors = plt.get_cmap("tab20")(labels / (max_label if max_label > 0 else 1))
 colors[labels < 0] = 0
 pcd.colors = o3d.utility.Vector3dVector(colors[:, :3])
-o3d.visualization.draw_geometries([pcd])
+# o3d.visualization.draw_geometries([pcd])
 
 
 # Remove small clusters
 noise_label = np.amax(labels)+1
 labels[labels == -1] = noise_label # need all non-negative for bincount
 count = np.bincount(labels)
-large_cluster_labels = np.unique(labels)[count > 500]
+large_cluster_labels = np.unique(labels)[count > 800]
 noise_points = np.argwhere(large_cluster_labels == noise_label)
 large_cluster_labels = np.delete(large_cluster_labels, noise_points) # remove noise points which might otherwise form a big cluster
 is_large_cluster = np.in1d(labels, large_cluster_labels)
@@ -64,16 +74,34 @@ o3d.visualization.draw_geometries([pcd])
 segment_models={}
 segments={}
 rest = pcd
-max_plane_idx = 10
+max_plane_idx = 15
 rest.paint_uniform_color([0.8, 0.8, 0.8])
 
 for i in range(max_plane_idx):
     colours = plt.get_cmap("tab20")(i)
-    segment_models[i], inliers = rest.segment_plane(
-    distance_threshold=0.07,ransac_n=3,num_iterations=1000)
+    try:
+        segment_models[i], inliers = rest.segment_plane(
+        distance_threshold=0.05,ransac_n=3,num_iterations=1000)
+    except:
+        break
+    
+    max_actual = i
     segments[i]=rest.select_by_index(inliers)
     segments[i].paint_uniform_color(list(colours[:3]))
     rest = rest.select_by_index(inliers, invert=True)
     print("pass",i+1,"/",max_plane_idx,"done.")
     
-o3d.visualization.draw_geometries([segments[i] for i in range(max_plane_idx)]+[rest])
+o3d.visualization.draw_geometries([segments[i] for i in range(max_actual)]+[rest], 
+    front=[0.01108164692705163, 0.99948318020971616, -0.030175645465445922],
+    lookat=[ 0.36654839499999969, 1.0939715543333333, 0.30774251988636392 ],
+    up=[ 0.34805089590732086, -0.032145887433672644, -0.93692433834286371 ],
+    zoom=0.37999999999999967
+    )
+
+
+# Flatten into 2D and downsample again
+points = np.asarray(pcd.points)
+points[:, 1] = 0.0 # flatten
+pcd.points = o3d.utility.Vector3dVector(points)
+pcd = pcd.voxel_down_sample(voxel_size=0.05)
+o3d.visualization.draw_geometries([pcd])
