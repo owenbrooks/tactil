298292ae739a081dd1f74fd5932813ef5cfa79e4
Owen Brooks <owen.h.brooks@gmail.com>
2022-03-18 11:14:15 +1100

    Neaten up gaussian map plotting

--- gaussian_map_plot.py
+++ gaussian_map_plot.py
@@ -13,12 +13,9 @@ print("Loaded pcd")
 pcd = pcd.voxel_down_sample(voxel_size=0.05)
 print("Downsampled pcd")
 
+# compute unit normal vectors
 normals = np.asarray(pcd.normals)
-print(normals.shape)
-print(np.sum(np.linalg.norm(normals)<1.0))
-
-# plot normals
-normals = normals[:10000, :]
+normals = normals[::100, :]
 magnitudes = np.linalg.norm(normals, axis=1)
 magnitudes[magnitudes==0] = 1e-6 # set 0 magnitude to very small value
 unit_normals = normals / magnitudes.reshape(-1, 1)
@@ -27,22 +24,10 @@ ny = unit_normals[:, 1]
 nz = unit_normals[:, 2]
 
 # plot in plotly
-# colorscales = px.colors.named_colorscales()
-# print(colorscales)
-scale = 'sunsetdark'
-# scale = 
-clr = scale
 fig = go.Figure(data=[go.Scatter3d(x=nx, y=ny, z=nz, mode='markers', 
         marker=dict(
             size=2,
-            color=np.linalg.norm(unit_normals, axis=1),                # set color to an array/list of desired values
-            colorscale='sunsetdark',   # choose a colorscale
-            colorbar=dict(
-            title="Colorbar"
-        ),
-            # opacity=0.8
+            color='#FF6692'
         ))
     ])
-                                   
-
 fig.show()
