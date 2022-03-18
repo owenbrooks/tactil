6bca1b0d724d86726d55d46d8343fc45b486ed60
Owen Brooks <owen.h.brooks@gmail.com>
2022-03-18 15:26:45 +1100

    Paint cluster labels onto original point cloud

--- gaussian_map_plot.py
+++ gaussian_map_plot.py
@@ -3,7 +3,7 @@ import open3d as o3d
 import sys
 import plotly.graph_objects as go
 from sklearn.cluster import MeanShift, estimate_bandwidth
-import plotly.express as px
+import matplotlib.pyplot as plt
 
 # Load pcd
 print("Loading pcd")
@@ -12,13 +12,14 @@ pcd = o3d.io.read_point_cloud(filename)
 print("Loaded pcd")
 
 # Downsample pcd
-pcd = pcd.voxel_down_sample(voxel_size=0.05)
+pcd = pcd.voxel_down_sample(voxel_size=0.25)
 print("Downsampled pcd")
 
 
 # Compute unit normal vectors
 normals = np.asarray(pcd.normals)
-normals = normals[::100, :]
+step = 1
+normals = normals[::step, :]
 magnitudes = np.linalg.norm(normals, axis=1)
 magnitudes[magnitudes==0] = 1e-6 # set 0 magnitude to very small value
 unit_normals = normals / magnitudes.reshape(-1, 1)
@@ -26,55 +27,38 @@ nx = unit_normals[:, 0]
 ny = unit_normals[:, 1]
 nz = unit_normals[:, 2]
 
-# Plot 3D gaussian map
-fig = go.Figure(data=[go.Scatter3d(x=nx, y=ny, z=nz, mode='markers', 
-        marker=dict(
-            size=2,
-            color='#FF6692'
-        ))
-    ])
-# fig.show()
 
-
-# Map to uv coordinates on unit sphere
-u = 0.5 + np.arctan2(nx, nz)
-v = 0.5 + np.arcsin(ny) / np.pi
-fig = go.Figure(data=[go.Scatter(x=u, y=v, mode='markers')])
-# fig.show()
-
-# Compute clustering with MeanShift
-X = np.stack([u, v], axis=1)
-X = unit_normals
-# The following bandwidth can be automatically detected using
-bandwidth = estimate_bandwidth(X, quantile=0.1)
-
+# Compute clustering with MeanShift after estimating bandwidth
+bandwidth = estimate_bandwidth(unit_normals, quantile=0.1)
 ms = MeanShift(bandwidth=bandwidth, bin_seeding=True)
-ms.fit(X)
+ms.fit(unit_normals)
 labels = ms.labels_
 cluster_centers = ms.cluster_centers_
-
 labels_unique = np.unique(labels)
 n_clusters_ = len(labels_unique)
+print("Number of estimated clusters : %d" % n_clusters_)
 
-print("number of estimated clusters : %d" % n_clusters_)
-
-
-# Plot 2D uv scatter plot
-print(labels.shape, labels, labels_unique)
-fig = go.Figure(data=[go.Scatter(x=u, y=v, mode='markers',
-    marker=dict(
-        color=labels,
-        size=6
-    ))])
-fig.show()
 
 # Plot 3D gaussian map
 fig = go.Figure(data=[
-        go.Scatter3d(x=nx, y=ny, z=nz, mode='markers', 
+        go.Scatter3d(x=nx[::10], y=ny[::10], z=nz[::10], mode='markers', 
             marker=dict(
-                color=labels,
+                color=labels[::10],
                 size=2,
             ),
         ), 
 ])
 fig.show()
+
+
+# Paint point cloud according to cluster
+clusters = []
+for i in range(len(labels_unique)):
+    colours = plt.get_cmap("tab20")(i)
+    current_cluster_mask = labels == i
+    current_cluster_indices = np.arange(len(np.asarray(pcd.points)))[current_cluster_mask]
+    cluster = pcd.select_by_index(current_cluster_indices)
+    cluster.paint_uniform_color(list(colours[:3]))
+    clusters.append(cluster)
+
+o3d.visualization.draw_geometries(clusters)
