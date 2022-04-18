4cc5e23deff55eea7e339368d4503c0d2659c385
Owen Brooks <owen.h.brooks@gmail.com>
2022-04-18 23:09:59 +1000

    Add route for processing pcd file

--- api/app.py
+++ api/app.py
@@ -1,18 +1,37 @@
 import os
-from flask import Flask, flash, request, redirect, url_for, send_from_directory
+from flask import Flask, flash, request, jsonify,  redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
+from process_cloud import process
 
 UPLOAD_FOLDER = './uploads'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'pcd'}
 MAX_CONTENT_LENGTH = 16 * 1000 * 1000
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = MAX_CONTENT_LENGTH
+app.debug = True
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/process', methods=['POST'])
+def process_file():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        json = request.json
+        print(json)
+    else:
+        return 'Content-Type not supported!'
+        
+    # TODO: validate filename
+    cloud_path = os.path.join('uploads', json['filename'])
+    process(cloud_path, visualise=False)
+
+    resp = jsonify({'message' : 'File successfully processed'})
+    resp.status_code = 200
+    return resp
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
