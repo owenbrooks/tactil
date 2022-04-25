17f0ef07530d89392bb0b53e675c093189908504
Owen Brooks <owen.h.brooks@gmail.com>
2022-04-25 23:08:19 +1000

    Add beginnings of editor

--- ui/src/interface/Edit.tsx
+++ ui/src/interface/Edit.tsx
@@ -1,33 +1,45 @@
 import { SyntheticEvent, useState } from 'react';
 import './Interface.css'
-import { Routes, Route, Link, useLocation } from "react-router-dom";
-import { LocationState, postData } from '../api';
-const generate_url = "http://localhost:5000/generate"
+import { BoxOutputs, postData } from '../api';
+import './Edit.css'
+const generate_url = "http://localhost:5000/generate";
 
-function Edit() {
+type EditProps = {
+  boxOutputs: BoxOutputs | undefined,
+  setBoxOutputs: React.Dispatch<React.SetStateAction<BoxOutputs | undefined>>
+}
 
-  const [generateFinished, setGenerateFinished] = useState(false);
+const minZoom = 0.1;
+const maxZoom = 50;
 
-  const handleGenerate = (_: SyntheticEvent) => {
-    const data = {"box_outputs": box_outputs};
-    postData(generate_url, data).then(response => {
-      console.log(response);
-      setGenerateFinished(true);
-    })
-  }
+function Edit(props: EditProps) {
 
-  const location = useLocation().state as LocationState;
-  const box_outputs = location?.box_outputs;
-  console.log(location)
+  const defaultNodes: Record<number, [number, number]> = {0: [0.0, 0.0], 1: [10.0, 10.0], 2: [-5, -8]};
+  const [zoomLevel, setZoomLevel] = useState(1.0);
+  const [nodes, setNodes] = useState<Record<number, [number, number]>>(defaultNodes);
+
+  const onScroll = (e: React.WheelEvent<HTMLDivElement>) => {
+    const delta = e.deltaY * -0.01;
+    const newZoom = zoomLevel + delta;
+    if (newZoom > minZoom && newZoom < maxZoom) {
+      setZoomLevel(newZoom);
+    }
+  };
 
   return (
-    <div className="interface">
-      <p>Edit</p>
-      <div>
-        <button onClick={handleGenerate}>Next</button>
+    <div className="edit-container">
+      {/* <p>Edit</p> */}
+      <div className='edit-params'>Edit</div>
+      <div className='edit-frame' onWheelCapture={onScroll} >
+        {Object.keys(nodes).map((nodeId) => {
+          const x = nodes[parseInt(nodeId)][0] * zoomLevel;
+          const y = nodes[parseInt(nodeId)][1] * zoomLevel;
+          const left = 'calc(' + x + 'px + 50%)';
+          const top = 'calc(' + y + 'px + 50%';
+          return <div className='node' style={{left: left, top: top, position:'absolute'}}></div>
+        })}
       </div>
-      {generateFinished && <a href="http://localhost:5000/generate/output" download target="_self">Download</a>}
-      <pre>{JSON.stringify(box_outputs, null, 2)}</pre>
+      {/* <pre>{JSON.stringify(props.boxOutputs, null, 2)}</pre> */}
     </div>
   );
 }
